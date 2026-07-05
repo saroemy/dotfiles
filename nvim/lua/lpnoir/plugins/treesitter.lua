1b6da1a83139f1fea8bbf3db17f2314d7856a8b3
Saroemy <persival90@gmail.com>
2026-07-05 23:54:03 +0200

    feat: migra nvim-treesitter al branch main per compatibilità con nvim 0.12

--- nvim/lua/lpnoir/plugins/treesitter.lua
+++ nvim/lua/lpnoir/plugins/treesitter.lua
@@ -1,9 +1,10 @@
 return { -- Highlight, edit, and navigate code
   'nvim-treesitter/nvim-treesitter',
+  branch = 'main',
   build = ':TSUpdate',
-  main = 'nvim-treesitter.configs', -- Sets main module to use for opts
-  opts = {
-    ensure_installed = {
+  lazy = false,
+  config = function()
+    local ensure_installed = {
       'bash',
       'c',
       'python',
@@ -13,39 +14,69 @@ return { -- Highlight, edit, and navigate code
       'lua',
       'luadoc',
       'markdown',
+      'markdown_inline',
       'query',
       'vim',
       'vimdoc',
       'php',
       'typescript',
       'twig',
-    },
-    auto_install = true,
-    incremental_selection = {
-      enable = true,
-      keymaps = {
-        init_selection = 'gnn',
-        scope_incremental = 'grc',
-        node_incremental = 'v',
-        node_decremental = 'V',
-      },
-    },
-    highlight = {
-      enable = true,
-      -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-      --  If you are experiencing weird indenting issues, add the language to
-      --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-      additional_vim_regex_highlighting = { 'ruby', 'lua', 'css', 'php' },
-    },
-    indent = { enale = true, disable = { 'ruby', 'lua', 'css', 'php' } },
-    -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
-    -- There are additional nvim-treesitter modules that you can use to interact
-    -- with nvim-treesitter. You should go explore a few and see what interests you:
-    --
-    --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-    --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-    --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
-  },
+    }
+    require('nvim-treesitter').install(ensure_installed)
+
+    -- Il branch main non ha più i moduli highlight/indent: si attivano per
+    -- buffer con le API native di Neovim.
+    local regex_highlighting = { ruby = true, lua = true, css = true, php = true }
+    local indent_disabled = { ruby = true, lua = true, css = true, php = true }
+
+    ---@param buf integer
+    ---@param language string
+    local function try_attach(buf, language)
+      if not vim.treesitter.language.add(language) then
+        return
+      end
+      vim.treesitter.start(buf, language)
+
+      -- Alcuni linguaggi si appoggiano ancora all'highlighting regex di vim
+      if regex_highlighting[language] then
+        vim.bo[buf].syntax = 'on'
+      end
+
+      if not indent_disabled[language] and vim.treesitter.query.get(language, 'indents') ~= nil then
+        vim.bo[buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+      end
+    end
+
+    local available_parsers = require('nvim-treesitter').get_available()
+    vim.api.nvim_create_autocmd('FileType', {
+      group = vim.api.nvim_create_augroup('lpnoir-treesitter', { clear = true }),
+      callback = function(args)
+        local buf, filetype = args.buf, args.match
+        local language = vim.treesitter.language.get_lang(filetype)
+        if not language then
+          return
+        end
+
+        if vim.tbl_contains(require('nvim-treesitter').get_installed 'parsers', language) then
+          try_attach(buf, language)
+        elseif vim.tbl_contains(available_parsers, language) then
+          -- auto-install dei parser mancanti alla prima apertura del filetype
+          require('nvim-treesitter').install(language):await(function()
+            try_attach(buf, language)
+          end)
+        else
+          try_attach(buf, language)
+        end
+      end,
+    })
+
+    -- Incremental selection: il modulo non esiste più sul branch main, si usa
+    -- quella nativa di Neovim 0.12 (`:help treesitter-incremental-selection`,
+    -- `an` espande / `in` riduce) mantenendo i tasti di prima.
+    vim.keymap.set('n', 'gnn', 'van', { remap = true, desc = 'Seleziona nodo treesitter' })
+    vim.keymap.set('x', 'v', 'an', { remap = true, desc = 'Espandi selezione al nodo padre' })
+    vim.keymap.set('x', 'V', 'in', { remap = true, desc = 'Riduci selezione al nodo figlio' })
+  end,
   init = function()
     -- Registra Twig come parser per i file Jinja
     vim.treesitter.language.register('twig', 'jinja')
